5c86e7373dfa656f4350247eebe127373e1f13d3
Semyon-Ivakaev <stixonforpython@gmail.com>
2020-07-01 22:30:26 +0300

    edit all repository

---
+++ src/sorted/puzirek/les3/Main.java
@@ -0,0 +1,31 @@
+package sorted.puzirek.les3;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            int x = (int) (Math.random() * 1000);
+            list.add(x);
+        }
+
+        System.out.println(list);
+
+        boolean cancel = false;
+
+        while (!cancel) {
+            cancel = true;
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (list.get(i) > list.get(i + 1)) {
+                    int a = list.get(i);
+                    list.set(i, list.get(i + 1));
+                    list.set(i + 1, a);
+                    cancel = false;
+                }
+            }
+        }
+        System.out.println(list);
+    }
+}
